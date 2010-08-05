e55cf77560d4826e603220564ba548f926ff706a
Alexander Duda <Alexander.Duda@dfki.de>
2010-08-05 16:15:37 +0200

    added step_back and decrement_stream no streams are deleted if an invalid step is performend

--- lib/simlog/stream_aligner.rb
+++ lib/simlog/stream_aligner.rb
@@ -8,6 +8,7 @@ module Pocosim
         attr_reader :current_streams
 	attr_reader :current_samples
 	attr_reader :next_samples
+        attr_reader :prev_samples
 
 	def initialize(use_rt = false, *streams)
 	    @use_rt  = use_rt
@@ -21,6 +22,7 @@ module Pocosim
             @current_samples = Array.new
             @next_samples    = Array.new
             @current_streams = Array.new
+            @prev_samples = Array.new
 
             min_stream, min_time = nil
             streams.each_with_index do |s, i|
@@ -43,33 +45,45 @@ module Pocosim
             nil
         end
 
-	def advance_stream(s, i)
-            if !next_samples[i]
-                current_samples.delete_at(i)
-                current_streams.delete_at(i)
-                next_samples.delete_at(i)
-                current_samples.each do |s|
-                    if s.index > i
-                        s.index -= 1
-                    end
-                end
-                next_samples.each do |s|
-                    if s.index > i
-                        s.index -= 1
-                    end
-                end
-                return
+        def decrement_stream(s,i)
+            if !prev_samples[i]
+              raise "Cannot decrement_stream #{s.name}. Beginning is reached"
             end
 
-	    current_samples[i] = next_samples[i]
+            next_samples[i] = current_samples[i]
+            current_samples[i] = prev_samples[i]
 
+            sample = s.previous
+            header = s.data_header
+	    if !sample || !header
+		prev_samples[i] = nil
+		return current_samples[i]
+	    end
+	
+	    time = current_streams[i].time
+            @time = if use_rt then time.first
+                    else time.last
+                    end
+
+            prev_samples[i] = StreamSample.new self.time, header.dup, s, i
+            current_samples[i]
+        end
+
+	def advance_stream(s, i)
+            if !next_samples[i]
+              raise "Cannot advance stream #{s.name}. End is reached"
+            end
+
+            prev_samples[i] = current_samples[i]
+	    current_samples[i] = next_samples[i]
+	   
 	    header = s.advance
 	    if !header
 		next_samples[i] = nil
 		return current_samples[i]
-	    end
-
-	    time = current_streams[i].time
+            end
+            
+            time = current_streams[i].time
             @time = if use_rt then time.first
                     else time.last
                     end
@@ -88,8 +102,22 @@ module Pocosim
         # single_data(stream_idx)
         def step
 	    min_sample = next_samples.compact.min { |s1, s2| s1.time <=> s2.time }
+            return nil if !min_sample
 	    advance_stream(min_sample.stream, min_sample.stream_index)
 	    return min_sample.stream_index, min_sample.time, single_data(min_sample.stream_index)
+        end
+
+        # Decrements one step in the joint stream, an returns the index of the
+        # updated stream as well as the time.
+        #
+        # The associated data sample can then be retrieved by
+        # single_data(stream_idx)
+
+        def step_back
+            max_sample = prev_samples.compact.max{ |s1, s2| s1.time <=> s2.time }
+            return nil if !max_sample
+	    decrement_stream(max_sample.stream, max_sample.stream_index)
+	    return max_sample.stream_index, max_sample.time, single_data(max_sample.stream_index)
         end
 
         # Returns the current data sample for the given stream index
