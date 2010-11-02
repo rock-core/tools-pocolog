7ebbf7d35613d65552c8fa4724c61a82789db969
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-11-02 12:25:24 +0100

    cleanup and test the logic in StreamAligner

--- lib/pocolog/stream_aligner.rb
+++ lib/pocolog/stream_aligner.rb
@@ -4,44 +4,40 @@ module Pocolog
 
 	attr_reader :use_rt
 	attr_reader :streams
-        attr_reader :current_pos
-        attr_reader :current_streams
+        attr_reader :sample_index
 	attr_reader :next_samples
 	attr_reader :current_samples
+        attr_reader :last_sample
         attr_reader :prev_samples
 
 	def initialize(use_rt = false, *streams)
 	    @use_rt  = use_rt
             @streams = streams
+            build_index
             rewind
         end
 
-        attr_reader :time
+        def time
+            if last_sample
+                last_sample.time
+            end
+        end
 
         def eof?
             @next_samples.compact.empty?
         end
 
 	def rewind
-            @current_streams = Array.new
             @prev_samples    = Array.new
-            @current_samples = Array.new
             @next_samples    = Array.new
-            @current_pos = -1
+            @current_samples = prev_samples
+            @last_sample     = nil
+            @sample_index    = -1
 
             streams.each_with_index do |s, i|
-		header = s.rewind
-                if !header   
-                    next_samples << nil
-                else    
-                  time = s.time
-                  time = if use_rt then time.first
-                       else time.last
-                       end
-		  next_samples    << StreamSample.new(time, header.dup, s, i)
-                end
-                current_streams << s
-	    end
+                s.rewind
+                advance_stream(s, i)
+            end
             nil
         end
 
@@ -54,44 +50,93 @@ module Pocolog
             reference = streams.max do |s1, s2|
                 s1.info.index.size <=> s2.info.index.size
             end
+            reference_index = reference.info.index
 
             index = Array.new
-            reference.info.index.each do |index_entry|
+            reference_index.each do |index_entry|
+                index_time = index_entry.last
                 positions = streams.map do |s|
+                    time_range = s.time_interval(use_rt)
                     if s == reference
                         index_entry.first
+                    elsif index_time < time_range[0]
+                        :before
+                    elsif index_time > time_range[1]
+                        :after
                     else
                         s.seek(index_entry.last)
+                        s.sample_index - 1
                     end
                 end
 
-                index << [positions.inject(&:+), positions]
+                index << [positions.find_all { |p| !p.respond_to?(:to_sym) }.inject(&:+), positions]
             end
             @index = index
         end
 
-        def seek_to_pos(pos)
-            if pos < 0 || pos > size
-                raise "#{pos} is out of bounds"
-            end
+        attr_reader :index
 
-            # For the aligner to function properly, we need to initialize the
-            # prev_samples and current_samples array. Seek to one before the
-            # target position to do that
-            target_pos = pos - 1
-            if target_pos > index.last[0]
-                entry = index.last
-            else
-                entry, _ = index.each_cons(2).find do |before, after|
-                    after[0] > target_pos
+        def preseek(entry)
+            # Forcefully switch to forward play direction
+            @current_samples = prev_samples
+            return if !entry
+
+            streams.each_with_index do |s, i|
+                positions = entry.last
+                case positions[i]
+                when :before
+                    s.rewind
+                    s.advance
+                    prev_samples[i] = nil
+                    next_samples[i] = create_stream_sample(s, i)
+                when :after
+                    s.last
+                    prev_samples[i] = create_stream_sample(s, i)
+                    next_samples[i] = nil
+                    s.next
+                else
+                    if positions[i] == 0
+                        s.rewind
+                        s.next
+                    else
+                        s.seek(positions[i])
+                    end
+                    next_samples[i] = create_stream_sample(s, i)
+                    advance_stream(s, i)
                 end
             end
-            streams.each_with_index do |s, i|
-                s.seek(entry[i + 1])
+            @sample_index = entry[0]
+            @last_sample = current_samples.compact.
+                max { |s1, s2| s1.time <=> s2.time }
+        end
+
+        def seek_to_pos(pos)
+            if pos < 0 || pos > size
+                raise OutOfBounds, "#{pos} is out of bounds"
             end
+
+            if pos < index.first[0]
+                rewind
+            elsif pos > index.last[0]
+                entry = index.last
+            elsif index.size == 1
+                entry = index.first
+            else
+                entry, _ = index.each_cons(2).find do |before, after|
+                    after[0] > pos
+                end
+            end
+
+            preseek(entry)
+
+            while @sample_index != pos
+                result = self.step
+            end
+            result
         end
 
         def seek_to_time(time)
+            raise NotImplementedError
         end
 
         def seek(pos_or_time)
@@ -102,35 +147,38 @@ module Pocolog
             end
         end
 
-        def decrement_stream(s,i)
-            next_samples[i], prev_samples[i] =
-                prev_samples[i], nil
+        # True if the last play operation was going forward
+        def playing_forward?;  current_samples.object_id == prev_samples.object_id end
+        # True if the last play operation was going backward
+        def playing_backward?; current_samples.object_id == next_samples.object_id end
 
-            s.previous
+        # Create a StreamSample instance that represents the current state of
+        # the given stream
+        def create_stream_sample(s, i)
             header = s.data_header
-	    if header
-                stream_time =
-                    if use_rt then header.rt
-                    else header.lg
-                    end
-                prev_samples[i] = StreamSample.new stream_time, header.dup, s, i
-            end
-            current_samples[i] = next_samples[i]
+            stream_time =
+                if use_rt then header.rt
+                else header.lg
+                end
+            StreamSample.new stream_time, header.dup, s, i
         end
 
-	def advance_stream(s, i)
-            prev_samples[i], next_samples[i] =
-                next_samples[i], nil
-
-	    header = s.advance
-	    if header
-                stream_time =
-                    if use_rt then header.rt
-                    else header.lg
-                    end
-                next_samples[i] = StreamSample.new stream_time, header.dup, s, i
+        # When playing in the backward direction, prev_samples is the lookahead,
+        # next_samples the current stream
+        def decrement_stream(s,i)
+            next_samples[i], prev_samples[i] = prev_samples[i], nil
+            if s.previous
+                prev_samples[i] = create_stream_sample(s, i)
             end
-            current_samples[i] = prev_samples[i]
+        end
+
+        # When playing in the forward direction, next_samples is the lookahead
+        # and prev_samples the current stream
+	def advance_stream(s, i)
+            prev_samples[i], next_samples[i] = next_samples[i], nil
+	    if s.advance
+                next_samples[i] = create_stream_sample(s, i)
+            end
 	end
 
         # call-seq:
@@ -141,15 +189,27 @@ module Pocolog
         #
         # The associated data sample can then be retrieved by
         # single_data(stream_idx)
-        def step	   
-            min_sample = next_samples.compact.min { |s1, s2| s1.time <=> s2.time }
+        def step
+            return if sample_index == size
+
+            # Check if we are changing the replay direction. If it is the case,
+            # we have to throw the first sample away as we are always reading
+            # with a one-sample lookahead
+            if playing_backward?
+                streams.each_with_index { |s, i| s.next }
+                if last_sample
+                    advance_stream(last_sample.stream, last_sample.stream_index)
+                end
+                @current_samples = prev_samples
+            end
+
+            @sample_index += 1
+            @last_sample = min_sample = next_samples.compact.min { |s1, s2| s1.time <=> s2.time }
             if !min_sample
                 return nil 
             end
 
-            @current_pos += 1
             advance_stream(min_sample.stream, min_sample.stream_index)
-	    @time = min_sample.time
             return min_sample.stream_index, min_sample.time,
                 single_data(min_sample.stream_index)
         end
@@ -159,25 +219,101 @@ module Pocolog
         #
         # The associated data sample can then be retrieved by
         # single_data(stream_idx)
-
         def step_back
-            max_sample = prev_samples.compact.max{ |s1, s2| s1.time <=> s2.time }
+            return if sample_index == -1
+
+            # Check if we are changing the replay direction. If it is the case,
+            # we have to throw the first sample away as we are always reading
+            # with a one-sample lookahead
+            if playing_forward?
+                streams.each_with_index { |s, i| s.previous }
+                if last_sample
+                    decrement_stream(last_sample.stream, last_sample.stream_index)
+                end
+                @current_samples = next_samples
+            end
+
+            @sample_index -= 1
+            @last_sample = max_sample = prev_samples.compact.max { |s1, s2| s1.time <=> s2.time }
             if !max_sample
                 return nil
             end
 
-            @current_pos -= 1
             decrement_stream(max_sample.stream, max_sample.stream_index)
-	    @time = max_sample.time
             return max_sample.stream_index, max_sample.time,
                 single_data(max_sample.stream_index)
         end
 
-        # Provided for backward compatibility only
-        def count_samples; size end
+        # call-seq:
+        #   aligner.next => time, time, [stream_index, data]
+        #
+        # Defined for compatibility with DataStream#next
+        def next
+            stream_index, time, data = step
+            if stream_index
+                return time, time, [stream_index, data]
+            end
+        end
 
+        def pretty_print(pp)
+            pp.text "playing " + (playing_forward? ? "forward" : "backward")
+            pp.breakable
+            pp.text "prev_samples:"
+            pp.nest(2) do
+                pp.breakable
+                pp.seplist(prev_samples.each_with_index) do |s, i|
+                    if s
+                        pp.text "#{i} #{s.time.to_f}"
+                    else
+                        pp.text "#{i} -"
+                    end
+                end
+            end
+            pp.breakable
+            pp.text "next_samples:"
+            pp.nest(2) do
+                pp.breakable
+                pp.seplist(next_samples.each_with_index) do |s, i|
+                    if s
+                        pp.text "#{i} #{s.time.to_f}"
+                    else
+                        pp.text "#{i} -"
+                    end
+                end
+            end
+            pp.breakable
+            pp.text "streams:"
+            pp.nest(2) do
+                pp.breakable
+                pp.seplist(streams.each_with_index) do |s, i|
+                    if s.eof?
+                        pp.text "#{i} eof"
+                    else
+                        pp.text "#{i} #{s.time.last.to_f}"
+                    end
+                end
+            end
+        end
+
+        # Defined for backward compatibility with DataStream#previous
+        def previous
+            stream_index, time, data = step_back
+            if stream_index
+                return time, time, [stream_index, data]
+            end
+        end
+
+        # Provided for backward compatibility only
+        def count_samples
+            STDERR.puts "WARN: StreamAligner#count_samples is deprecated. Use #size instead"
+            size
+        end
+
+        # Returns the number of samples one can get out of this stream aligner
+        #
+        # This is the sum of samples available on each of the underlying streams
         def size
-            streams.map(&:size).inject(&:+)
+            streams.inject(0) { |result, s| result += s.size }
         end
 
         # Returns the current data sample for the given stream index
---
+++ test/test_stream_aligner.rb
@@ -0,0 +1,116 @@
+require 'pocolog'
+require 'test/unit'
+
+class TC_StreamAligner < Test::Unit::TestCase
+    attr_reader :logfile
+    attr_reader :stream
+
+    def create_fixture
+        logfile = Pocolog::Logfiles.create('test')
+        all_values  = logfile.stream('all', 'int', true)
+        odd_values  = logfile.stream('odd', 'int', true)
+        even_values = logfile.stream('even', 'int', true)
+
+        @interleaved_data = []
+        100.times do |i|
+            all_values.write(Time.at(i * 1000), Time.at(i), i)
+            interleaved_data << i
+            if i % 2 == 0
+                even_values.write(Time.at(i * 1000, 500), Time.at(i, 500), i * 100)
+                interleaved_data << i * 100
+            else
+                odd_values.write(Time.at(i * 1000, 500), Time.at(i, 500), i * 10000)
+                interleaved_data << i * 10000
+            end
+        end
+        logfile.close
+    end
+
+    # The data stream as the StreamAligner should give to us
+    attr_reader :interleaved_data
+    def setup
+        create_fixture
+        @logfile = Pocolog::Logfiles.open('test.0.log')
+        @stream  = Pocolog::StreamAligner.new(false, logfile.stream('all'), logfile.stream('odd'), logfile.stream('even'))
+    end
+
+    def teardown
+        FileUtils.rm_f 'test.0.log'
+        FileUtils.rm_f 'test.0.idx'
+    end
+
+    def test_properties
+        assert !stream.eof?
+        assert_equal 200, stream.size
+    end
+
+    def test_playing_forward_backward
+        assert stream.playing_forward?
+        stream.next
+        assert stream.playing_forward?
+        stream.previous
+        assert stream.playing_backward?
+        stream.next
+        assert stream.playing_forward?
+    end
+
+    def test_step_by_step_all_the_way
+        2.times do
+            stream_indexes, sample_indexes, all_data = Array.new, Array.new, Array.new
+            while (data = stream.step) && (all_data.size <= interleaved_data.size * 2)
+                stream_indexes  << data[0]
+                sample_indexes << stream.sample_index
+                all_data << data[2]
+            end
+            # verify that calling #step again is harmless
+            assert !stream.step 
+            assert stream.eof?
+            assert_equal 200, stream.sample_index
+            assert_equal interleaved_data, all_data
+            assert_equal [[0, 2, 0, 1]].to_set, stream_indexes.each_slice(4).to_set
+            assert_equal (0...200).to_a, sample_indexes
+
+            # Now go back
+            stream_indexes, sample_indexes, all_data = Array.new, Array.new, Array.new
+            while (data = stream.step_back) && (all_data.size <= interleaved_data.size * 2)
+                stream_indexes  << data[0]
+                sample_indexes << stream.sample_index
+                all_data << data[2]
+            end
+            assert_equal interleaved_data, all_data.reverse
+            assert_equal [[0, 2, 0, 1]].to_set, stream_indexes.reverse.each_slice(4).to_set
+            assert_equal (0...200).to_a, sample_indexes.reverse
+        end
+    end
+
+    # Checks that stepping and stepping back in the middle of the stream works
+    # fine
+    def test_step_by_step_middle
+        assert_equal [0, Time.at(0), 0], stream.step
+        assert_equal [2, Time.at(0, 500), 0], stream.step
+        assert_equal [0, Time.at(1), 1], stream.step
+        assert_equal [1, Time.at(1, 500), 10000], stream.step
+        assert_equal [0, Time.at(1), 1], stream.step_back
+        assert_equal [2, Time.at(0, 500), 0], stream.step_back
+        assert_equal [0, Time.at(1), 1], stream.step
+        assert_equal [1, Time.at(1, 500), 10000], stream.step
+    end
+
+    # Tests seeking on an integer position
+    def test_seek_at_position
+        sample = stream.seek(10)
+        assert_equal 10, stream.sample_index
+        assert_equal Time.at(5), stream.time
+        assert_equal [0, Time.at(5), 5], sample
+
+        # Check that seeking did not break step / step_back
+        assert_equal [1, Time.at(5, 500), 50000], stream.step
+        assert_equal [0, Time.at(6), 6], stream.step
+        assert_equal [2, Time.at(6, 500), 600], stream.step
+        sample = stream.seek(10)
+        assert_equal [2, Time.at(4, 500), 400], stream.step_back
+        assert_equal [0, Time.at(4), 4], stream.step_back
+        assert_equal [1, Time.at(3, 500), 30000], stream.step_back
+    end
+end
+
