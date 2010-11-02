431d70350c6171836d83a2f388cddf37e11e3512
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2010-11-02 12:37:41 +0100

    fix backward compatibility file simlog.rb

--- lib/simlog.rb
+++ lib/simlog.rb
@@ -1,3 +1,3 @@
 STDERR.puts "require 'simlog' and the Pocosim namespaces are deprecated. Use require 'pocolog' and the Pocolog namespace instead"
-require 'simlog'
+require 'pocolog'
 Pocosim = Pocolog
